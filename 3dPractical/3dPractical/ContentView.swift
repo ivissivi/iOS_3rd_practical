7b51a1964fe8bcbf58f96b4c657f3e76c2e872bc
ivissivi <ivis31121999@gmail.com>
2022-03-12 13:23:32 +0100

    Add files via upload

---
+++ 3dPractical/3dPractical/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  3dPractical
+//
+//  Created by user215333 on 3/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 3dPractical/3dPractical/_dPracticalApp.swift
@@ -0,0 +1,17 @@
+//
+//  _dPracticalApp.swift
+//  3dPractical
+//
+//  Created by user215333 on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct _dPracticalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
